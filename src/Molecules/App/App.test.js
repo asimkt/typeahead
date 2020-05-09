ef7eac573b11812129ac407e510393b4ef5a202f
Asim K T <asim@quintype.com>
2020-05-09 12:31:38 +0530

    Folder structure

--- src/Molecules/App/App.test.js
+++ src/Molecules/App/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Accessible TypeAhead content', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Accessible TypeAhead/i);
   expect(linkElement).toBeInTheDocument();
 });
