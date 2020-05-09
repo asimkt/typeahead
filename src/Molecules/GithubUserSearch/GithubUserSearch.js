aad30c05dd9f401b336d294e81c3707fa098882f
Asim K T <asim@quintype.com>
2020-05-09 13:59:25 +0530

    create an opts prop for scalability

--- src/Molecules/GithubUserSearch/GithubUserSearch.js
+++ src/Molecules/GithubUserSearch/GithubUserSearch.js
@@ -2,9 +2,14 @@ import React from 'react';
 import TypeAhead from '../TypeAhead/TypeAhead';
 
 function GithubUserSearch() {
+  const opts = {
+    optionKey: 'login',
+    label: 'Github Users: ',
+    errorMsg: 'Please select a github user',
+  };
   return (
     <div className="GithubUserSearch">
-      <TypeAhead apiPrefix="https://api.github.com/search/users?q=" optionKey="login" label="Github Users: "/>
+      <TypeAhead apiPrefix="https://api.github.com/search/users?q=" opts={opts} />
     </div>
   );
 }
--- src/Molecules/TypeAhead/TypeAhead.js
+++ src/Molecules/TypeAhead/TypeAhead.js
@@ -3,16 +3,19 @@ import debounce from 'lodash/debounce';
 import './TypeAhead.css';
 // Component will do some kind of caching in Browser.
 // The server should cache the apiPrefix also to handle millions of requests per hour.
-function TypeAhead({ apiPrefix, name = 'default', label, optionKey }) {
-  const onSubmit = () => {
-    console.log('Form submitted');
-  };
+function TypeAhead({ apiPrefix, name = 'default', onOptionSelect, opts }) {
+  const { errorMsg = 'Please select an option', optionKey, label } = opts;
   const [options, setOptions] = useState([]);
   const [inputValue, setInputValue] = useState('');
+  const [active, setActive] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
   const debouncedApiCall = useCallback(
     debounce(async value => {
       if (apiPrefix) {
+        setLoading(true);
         const response = await (await fetch(`${apiPrefix}${value}`)).json();
+        setLoading(false);
         if (response.items && Symbol.iterator in Object(response.items)) setOptions(response.items);
       }
     }, 500),
@@ -22,6 +25,15 @@ function TypeAhead({ apiPrefix, name = 'default', label, optionKey }) {
     setInputValue(e.target.value);
     debouncedApiCall(e.target.value);
   };
+  const onSubmit = e => {
+    e.preventDefault();
+    if (!active) {
+      setError(errorMsg);
+      return;
+    }
+    console.log('Form submitted', active);
+    typeof onOptionSelect === 'function' && onOptionSelect(active);
+  };
   return (
     <div className="TypeAhead">
       <form onSubmit={onSubmit}>
@@ -30,7 +42,9 @@ function TypeAhead({ apiPrefix, name = 'default', label, optionKey }) {
       </form>
       <ul className="suggestions">
         {options.slice(0, 10).map((option, index) => (
-          <li key={index}>{optionKey ? option[optionKey] : option}</li>
+          <li key={index} onHover={() => setActive(option)}>
+            {optionKey ? option[optionKey] : option}
+          </li>
         ))}
       </ul>
     </div>
