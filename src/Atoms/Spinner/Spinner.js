3fe133cd453becc0f5ad62e56c0dde8584100559
Asim K T <asim@quintype.com>
2020-05-10 11:34:47 +0530

    Add spinner component

---
+++ src/Atoms/Spinner/Spinner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Spinner.css';
+export const Spinner = () => (
+  <div className="loadingio-spinner-spinner-weetwmx5lke">
+    <div className="ldio-jntu9ecm4m">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+);
